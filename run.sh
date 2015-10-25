bc206952d92a865c53f99671f540c4d945c82116
a-magdy <magdy.a@hotmail.com>
2015-10-25 22:13:53 +0200

    Adding a remote mongodb instance

---
+++ run.sh
@@ -0,0 +1,1 @@
+MONGO_URL=mongodb://ahmad:ahmad@ds041494.mongolab.com:41494/parlote meteor run
---
+++ server/lib/statup.js
@@ -0,0 +1,3 @@
+Meteor.startup(function(){
+  //process.env.MONGO_URL = 'mongodb://ahmad:ahmad@ds041494.mongolab.com:41494/parlote';
+});
